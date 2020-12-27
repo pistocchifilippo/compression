b3cd1f623a5b5e9f042c5edaeb16b75dceb0353f
Filippo Pistocchi <filippo.pistocchi4@studio.unibo.it>
2020-12-27 15:00:57 +0100

    Refactored modifiable tree

--- src/structures/BinaryTree.scala
+++ src/structures/BinaryTree.scala
@@ -1,8 +1,6 @@
 package structures
 
-import structures.BinaryTree.{BinaryTree, Branch, Leaf, Root}
-
-import java.util.function.BinaryOperator
+import structures.BinaryTree.ModifiableBinaryTree.{MBranch, MLeaf}
 
 object BinaryTree {
 
@@ -24,57 +22,57 @@ object BinaryTree {
     override def one: Option[BinaryTree] = None
   }
 
-
-
-
-//  /** Makes a balanced tree
-//   *
-//   * @param height
-//   * @return
-//   */
-//  def apply(height: Int): BinaryTree =
-//    if (height == 0) Leaf() else Branch(apply(height - 1), apply(height - 1))
-
-
-
-
   def addLayer(tree: BinaryTree): BinaryTree = ???
 
 
+
+  object ModifiableBinaryTree {
+
+    trait ModifiableBinaryTree extends BinaryTree {
+      private var _prev: Option[BinaryTree] = None
+      private var _zero: Option[BinaryTree] = None
+      private var _one: Option[BinaryTree] = None
+
+      override def prev: Option[BinaryTree] = _prev
+
+      override def zero: Option[BinaryTree] = _zero
+
+      override def one: Option[BinaryTree] = _one
+
+      def setPrev(prev: BinaryTree): Unit = {_prev = Some(prev)}
+
+      def setZero(zero: BinaryTree): Unit = {_zero = Some(zero)}
+
+      def setOne(one: BinaryTree): Unit = {_one = Some(one)}
+
+      def toBinaryTree: BinaryTree
+    }
+
+    /** Makes a modifiable balanced tree
+     *
+     * @param height of the tree
+     * @return the modifiable tree
+     */
+    def apply(height: Int): BinaryTree =
+      if (height == 0) MLeaf() else MBranch(Some(apply(height - 1)), Some(apply(height - 1)))
+
+    case class MRoot() extends ModifiableBinaryTree {
+      override def toBinaryTree: BinaryTree = Root(this.zero, this.one)
+    }
+    case class MBranch(override val zero: Option[BinaryTree], override val one: Option[BinaryTree]) extends ModifiableBinaryTree {
+      def this() = {
+        this(None, None)
+      }
+      override def toBinaryTree: BinaryTree = Branch(this.prev, this.zero, this.one)
+    }
+    case class MLeaf() extends ModifiableBinaryTree {
+      override def toBinaryTree: BinaryTree = Leaf(this.prev)
+    }
+
+  }
+
 }
 
-object Build {
 
-  trait ModifiableBinaryTree extends BinaryTree {
-    private var _prev: Option[BinaryTree] = None
-    private var _zero: Option[BinaryTree] = None
-    private var _one: Option[BinaryTree] = None
-
-    override def prev: Option[BinaryTree] = _prev
-
-    override def zero: Option[BinaryTree] = _zero
-
-    override def one: Option[BinaryTree] = _one
-
-    def setPrev(prev: BinaryTree): Unit = {_prev = Some(prev)}
-
-    def setZero(zero: BinaryTree): Unit = {_zero = Some(zero)}
-
-    def setOne(one: BinaryTree): Unit = {_one = Some(one)}
-
-    def toBinaryTree: BinaryTree
-  }
-
-  case class MRoot() extends ModifiableBinaryTree {
-    override def toBinaryTree: BinaryTree = Root(this.zero, this.one)
-  }
-  case class MBranch() extends ModifiableBinaryTree {
-    override def toBinaryTree: BinaryTree = Branch(this.prev, this.zero, this.one)
-  }
-  case class MLeaf() extends ModifiableBinaryTree {
-    override def toBinaryTree: BinaryTree = Leaf(this.prev)
-  }
-
-}
 
 
